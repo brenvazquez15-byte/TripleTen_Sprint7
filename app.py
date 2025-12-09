15949d281b99c61e2a468a030b3e314f75ec0547
brenvazquez15-byte <bren.vazquez15@gmail.com>
2025-12-08 22:13:57 -0600

    creación de botones para generar graficos

--- app.py
+++ app.py
@@ -4,3 +4,37 @@ import plotly.graph_objects as go
 
 # Leer los datos del archivo CSV
 df = pd.read_csv('vehicles_us.csv')
+
+# Encabezado
+st.header("Ánalisis de datos de anuncion de venta de autos")
+
+# Checkboxes para seleccionar los gráficos a mostrar
+hist_boton = st.button('Construir histograma')
+disp_boton = st.button('Construir gráfico de dispersión')
+
+# Lógica a ejecutar cuando se hace clic en el boton de histograma
+if hist_boton:
+    # Escribir un mensaje en la aplicación
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=df['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_boton:
+    # Crear un scatter plot utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de scatter
+    fig_2 = go.Figure(data=[go.Scatter(x=df['odometer'], y=df['price'], mode='markers')])
+
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig_2.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    st.plotly_chart(fig_2, use_container_width=True)
